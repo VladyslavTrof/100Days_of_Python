8ce058b63061ee16f0d94b68ba5e60c71622f101
Vladyslav <151214597+VladyslavTrof@users.noreply.github.com>
2024-05-28 19:24:12 +0100

    Day 9 Dictionaries Completed Code

--- day_9.py
+++ day_9.py
@@ -1,1 +1,58 @@
-# to be completed
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary["Function"])
+
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
+
+
+empty_dictionary = {}
+
+
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+
+#######################################
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+print(travel_log)
+
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+print(travel_log)
+
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+    },
+]
+print(travel_log)
